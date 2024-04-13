ad173dd8a24dfb7faf9b5df8a07bd2a3b68c2dfa
Gordon Steemson <gsteemso@gmail.com>
2024-04-12 20:40:30 -0700

    Tentatively finalized formula.  Needs extensive testing.

--- Library/Formula/gmp.rb
+++ Library/Formula/gmp.rb
@@ -13,10 +13,7 @@ class Gmp < Formula
   option :universal
 
   def install
-    ENV.cxx11 if build.cxx11?
-    ENV.permit_arch_flags if build.universal?
-
-    # a utility routine to map Tigerbrew’s CPU symbols to those used in configuring a GMP build
+    # utility routine:  map Tigerbrew’s CPU symbols to those for configuring a GMP build
     def cpu_lookup(cpu_sym)
       case cpu_sym
         when :g3
@@ -40,18 +37,15 @@ class Gmp < Formula
       end
     end
 
-    # a utility routine for processing diffs (used when splicing together the architecture-
-    # dependent header files of a :universal build)
-    def extract_new_lines(hunk)
-      hunk.lines.select { |l| l =~ /^\+/ }.map { |l| l[1, -1] }
-    end
-
-    b_cpu = Hardware::CPU.family
+    build_cpu = Hardware::CPU.family
     tuple_trailer = "apple-darwin#{`uname -r`}"
 
+    ENV.cxx11 if build.cxx11?
+
     if build.universal?
+      ENV.permit_arch_flags
       archs = Hardware::CPU.universal_archs
-      mkdir per_arch_gmp_h
+      mkdir 'per_arch_gmp_h'
     elsif MacOS.prefer_64_bit?
       archs = [Hardware::CPU.arch_64_bit]
     else
@@ -63,15 +57,7 @@ class Gmp < Formula
     archs.each do |arch|
       ENV.append_to_cflags "-arch #{arch}"
 
-      h_sym = (build.bottle? ? (ARGV.bottle_arch or Hardware.oldest_cpu) : b_cpu)
-
-      args = %W[
-        --prefix=#{prefix}
-        --enable-cxx
-        --build=#{cpu_lookup(b_cpu)}-#{tuple_trailer}
-        --host=#{cpu_lookup(h_sym)}-#{tuple_trailer}
-      ]
-      args << '--disable-silent-rules' if ARGV.verbose?
+      host_sym = (build.bottle? ? (ARGV.bottle_arch or Hardware.oldest_cpu) : build_cpu)
 
       case arch
         when :i386, :ppc
@@ -91,14 +77,23 @@ class Gmp < Formula
       mkdir dir
       cd dir
 
-      ENV.deparallelize
-      system '../configure', "--exec-prefix=#{Dir.pwd}", *args
+      args = %W[
+        --prefix=#{prefix}
+        --exec-prefix=#{Dir.pwd}
+        --enable-cxx
+        --build=#{cpu_lookup(build_cpu)}-#{tuple_trailer}
+        --host=#{cpu_lookup(host_sym)}-#{tuple_trailer}
+      ]
+      args << (ARGV.verbose? ? '--disable-silent-rules' : '--enable-silent-rules')
+
+      system '../configure', *args
       system 'make'
       system 'make', 'check'
+      ENV.deparallelize
       system 'make', 'install'
 
-      # this header is architecture-dependent; in the case of installing :universal, this copy will
-      # be used when merging them all together
+      # gmp.h is architecture-dependent; in the case of installing :universal, this copy will be
+      # used when merging them all together
       system 'cp', 'include/gmp.h', "../per_arch_gmp_h/#{arch}"
 
       # undo architecture-specific tweaks before next run
@@ -113,36 +108,41 @@ class Gmp < Formula
 
     if build.universal?
       # build the fat libraries directly into place
-      system 'lipo', '-create', *Dir["{#{dirs.join(',')}}/lib/libgmp.??.dylib"],
+      system 'lipo', '-create', *Dir["{#{dirs.join(',')}}/lib/libgmp.10.dylib"],
                      '-output', lib/'libgmp.10.dylib'
       system 'lipo', '-create', *Dir["{#{dirs.join(',')}}/lib/libgmp.a"],
                      '-output', lib/'libgmp.a'
-      system 'lipo', '-create', *Dir["{#{dirs.join(',')}}/lib/libgmpxx.?.dylib"],
+      system 'lipo', '-create', *Dir["{#{dirs.join(',')}}/lib/libgmpxx.4.dylib"],
                      '-output', lib/'libgmpxx.4.dylib'
       system 'lipo', '-create', *Dir["{#{dirs.join(',')}}/lib/libgmpxx.a"],
                      '-output', lib/'libgmpxx.a'
       # grab the symlinks too
       lib.install Dir["#{dirs.first}/lib/lib*"].select { |s| Pathname.new(s).symlink? }
-      # and the C++ header, which is architecture-independent
-      include.install "#{dirs.first}/include/gmpxx.h"
 
       # The system-specific gmp.h files need to be surgically combined.  They were stashed in
-      # ./per_arch_gmp_h/ for this purpose.  The differences are minor and can be #ifdef’d together.
+      # ./per_arch_gmp_h/ for this purpose.  The differences are minor and can be “#if defined ()”
+      # together.
       basis_file = "per_arch_gmp_h/#{archs.first}"
       diffpoints = {}  # Keyed by line number in the basis file.  Contains arrays of two-element
                        # hashes, where one item is the arch and the other is the applicable hunk.
       archs[1..-1].each do |a|
-        raw_diffs = `diff -u 0 #{basis_file} per_arch_gmp_h/#{a}`
-        diff_hunks = raw_diffs.lines[2..-1].join('').split(/(?=^\@\@)/)
+        raw_diffs = `diff --unified=0 #{basis_file} per_arch_gmp_h/#{a}`
+        diff_hunks = raw_diffs.lines[2..-1].join('').split(/(?=^@@)/)
         diff_hunks.each do |d|
-          basis_line = d.match(/\A@@ -(\d+)/)[0]
-          unless diffpoints.has_key?(basis_line)
-            diffpoints[basis_line] = []
+          base_linenumber_string = ('00000' + d.match(/\A@@ -(\d+)/)[1])[-5..-1]
+          unless diffpoints.has_key?(base_linenumber_string)
+            diffpoints[base_linenumber_string] = []
           end
-          diffpoints[basis_line] << { :arch => a,
-                                      :displacement => d.match(/\A@@ -\d+,(\d+)/)[0],
-                                      :hunk_lines => extract_new_lines(d)
-                                    }
+          length_match = d.match(/\A@@ -\d+,(\d+)/)
+          # if the hunk length is 1 the comma and second number are not present
+          length_match = (length_match == nil ? 1 : length_match[1].to_i)
+          line_group = []
+          d.lines { |line| line_group << line[1..-1] if line =~ /^\+/ }
+          diffpoints[base_linenumber_string] << {
+            :arch => a,
+            :displacement => length_match,
+            :hunk_lines => line_group
+          }
         end
       end
       # Ideally the algorithm would account for overlapping and/or different-length hunks at this
@@ -150,28 +150,31 @@ class Gmp < Formula
       # and would in any case only become relevant if "REALLY universal" triple-or-more fat
       # binaries are implemented, it can wait.
 
-      basis_lines = basis_file.lines  # the array indices are one less than the line numbers
+      basis_lines = []
+      File.open(basis_file, 'r') { |text| basis_lines = text.read.lines[0..-1] }
+      # bear in mind that the line-array indices are one less than the line numbers
+
       # start with the last diff point so that the insertions don't screw up our line numbering
-      diffpoints.keys.sort.reverse.each do |index|
-        diff_start = index - 1
-        diff_end = index + diffpoints[index][0][:displacement] - 2
-        basis_lines[diff_start..diff_end] = [
+      diffpoints.keys.sort.reverse.each do |index_string|
+        diff_start = index_string.to_i - 1
+        diff_end = index_string.to_i + diffpoints[index_string][0][:displacement] - 2
+        adjusted_lines = [
           "\#if defined (__#{archs.first}__)\n",
           basis_lines[diff_start..diff_end],
-          *diffpoints[index].each { |d|
-            [ "\#elif defined (__#{d[:arch]}__)\n", *d[:hunk_lines] ]
-          },
+          *(diffpoints[index_string].map { |d| [ "\#elif defined (__#{d[:arch]}__)\n", *(d[:hunk_lines]) ] }),
           "\#endif\n"
         ]
+        basis_lines[diff_start..diff_end] = adjusted_lines
       end
-      File.new(include/'gmp.h'.to_path, 'w', 0644).write basis_lines.join('')
+
+      File.new((include/'gmp.h').to_path, 'w', 0644).write basis_lines.join('')
     else
       lib.install Dir["#{dirs.first}/lib/lib*"]
-      include.install Dir["#{dirs.first}/include/*"]
+      include.install "#{dirs.first}/include/gmp.h"
     end
 
-    # install & fix up the pkgconfig files, which still expect the libraries in the build directory
-    # after they have been moved:
+    # install & fix up the pkgconfig files, which still expect the libraries to be in the build
+    # directory after they have been moved:
     lib.install "#{dirs.first}/lib/pkgconfig"
     Dir["#{lib}/pkgconfig/*"].each do |f|
       system 'sed', '-e', '/^exec_prefix=/d', '-e', 's/exec_prefix/prefix/g', f
