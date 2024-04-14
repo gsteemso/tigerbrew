073331bc415c0461fece5152eff68439daaa9cce
Gordon Steemson <gsteemso@gmail.com>
2024-04-14 14:08:55 -0700

    c-ares:  incomplete edits to enable --universal.  Turns out the headers are arch-dependent; needs work.

--- Library/Formula/c-ares.rb
+++ Library/Formula/c-ares.rb
@@ -14,11 +14,104 @@ class CAres < Formula
     sha1 "27494a19ac612daedeb55356e911328771f94b19" => :mountain_lion
   end
 
+  option :universal
+
+  # `class_exec` doesn't exist in Tiger/Leopard stock Ruby.  Ideally, find a workaround
+  Pathname.class_exec {
+    # binread does not exist in Leopard stock Ruby 1.8.6, and Tigerbrew's
+    # cobbled-in version doesn't work, so use this instead
+    def b_read(offset = 0, length = self.size)
+      self.open('rb') do |f|
+        f.pos = offset
+        f.read(length)
+      end
+    end
+
+    def is_bare_mach_o?
+      # MH_MAGIC    = 'feedface'
+      # MH_MAGIC_64 = 'feedfacf' -- same value with lowest-order bit inverted
+      [self.b_read(0,4).unpack('N').first & 0xfffffffe].pack('N').unpack('H8').first == 'feedface'
+    end
+  }
+
   def install
-    system "./configure", "--prefix=#{prefix}",
-                          "--disable-dependency-tracking",
-                          "--disable-debug"
-    system "make", "install"
+    def scour_keg(stash, sub_path)
+      s_p = (sub_path == '' ? '' : sub_path + '/')
+      Dir["#{prefix}/#{s_p}*"].each do |f|
+        pn = Pathname(f)
+        spb = s_p + pn.basename
+        if pn.directory?
+          mkdir "#{stash}/#{spb}"
+          scour_keg(stash, spb)
+        elsif ((not pn.symlink?) and pn.file? and pn.is_bare_mach_o?)
+          cp pn, "#{stash}/#{spb}"
+        end
+      end
+    end
+
+    def merge_mach_o_stashes(arch_dirs, sub_path)
+      s_p = (sub_path == '' ? '' : sub_path + '/')
+      Dir["#{arch_dirs.first}/#{s_p}*"].each do |f|
+        pn = Pathname(f)
+        spb = s_p + pn.basename
+        if pn.directory?
+          merge_mach_o_stashes(arch_dirs, spb)
+        else
+          system 'lipo', '-create', *Dir["{#{arch_dirs.join(',')}}/#{spb}"],
+                         '-output', prefix/spb
+        end
+      end
+    end
+
+    if build.universal?
+      ENV.permit_arch_flags
+      archs = Hardware::CPU.universal_archs
+    elsif MacOS.prefer_64_bit?
+      archs = [Hardware::CPU.arch_64_bit]
+    else
+      archs = [Hardware::CPU.arch_32_bit]
+    end
+
+    dirs = []
+
+    archs.each do |arch|
+      if build.universal?
+        ENV.append_to_cflags "-arch #{arch}"
+        case arch
+          when :i386, :ppc then ENV.m32
+          when :x86_64, :ppc64 then ENV.m64
+        end
+        dir = "build-#{arch}"
+        dirs << dir
+        mkdir dir
+      end
+
+      system "./configure", "--prefix=#{prefix}",
+                            "--disable-dependency-tracking",
+                            "--disable-debug",
+                            "--enable-symbol-hiding"
+      system "make", "install"
+
+      if build.universal?
+        # undo architecture-specific tweaks before next run
+        ENV.remove_from_cflags "-arch #{arch}"
+        case arch
+          when :i386, :ppc
+            ENV.remove 'HOMEBREW_ARCHFLAGS', '-m32'
+            unless superenv?
+              ENV.remove 'LDFLAGS', "-arch #{Hardware::CPU.arch_32_bit}"
+            end
+          when :x86_64, :ppc64
+            ENV.remove "HOMEBREW_ARCHFLAGS", "-m64"
+            unless superenv?
+              ENV.remove 'LDFLAGS', "-arch #{Hardware::CPU.arch_64_bit}"
+            end
+        end
+        scour_keg(dir, '')
+        system 'make', 'clean'
+      end
+    end
+    merge_mach_o_stashes(dirs, '') if build.universal?
   end
 
   test do
@@ -33,6 +126,7 @@ class CAres < Formula
         return 0;
       }
     EOS
+    ENV.universal_binary if build.universal?
     system ENV.cc, "test.c", "-L#{lib}", "-lcares", "-o", "test"
     system "./test"
   end
