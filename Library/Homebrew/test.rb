8e1630c0c8c762e0cc229954392f00b3522ea4db
Gordon Steemson <gsteemso@gmail.com>
2024-05-04 23:40:25 -0700

    test.rb:  edits to bring test & build environments more in line

--- Library/Homebrew/test.rb
+++ Library/Homebrew/test.rb
@@ -14,23 +14,21 @@ begin
   error_pipe = UNIXSocket.open(ENV["HOMEBREW_ERROR_PIPE"], &:recv_io)
   error_pipe.fcntl(Fcntl::F_SETFD, Fcntl::FD_CLOEXEC)
 
-  if superenv?
-    ENV.extend(Superenv)
-  else
-    ENV.extend(Stdenv)
-  end
+  ENV.activate_extensions!
   ENV.setup_build_environment
 
   trap("INT", old_trap)
 
   formula = ARGV.formulae.first
   formula.extend(Homebrew::Assertions)
-  formula.extend(Debrew::Formula) if ARGV.debug?
-
-  # tests can also return false to indicate failure
-  Timeout.timeout TEST_TIMEOUT_SECONDS do
-    raise "test returned false" if formula.run_test == false
-  end
+  if ARGV.debug?
+    formula.extend(Debrew::Formula)
+  else
+    # tests can also return false to indicate failure
+    Timeout.timeout TEST_TIMEOUT_SECONDS do
+      raise "test returned false" if formula.run_test == false
+    end # timeout?
+  end # debug?
 rescue Exception => e
   Marshal.dump(e, error_pipe)
   error_pipe.close
