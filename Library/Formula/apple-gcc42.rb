a1571b3917f1be2616954e035635bde46a5b3bb1
Gordon Steemson <gsteemso@gmail.com>
2024-05-07 08:30:25 -0700

    apple-gcc42:  significant modernization edits to boilerplate

--- Library/Formula/apple-gcc42.rb
+++ Library/Formula/apple-gcc42.rb
@@ -1,5 +1,3 @@
-require 'formula'
-
 class TigerOnly < Requirement
   def message; <<-EOS.undent
     gcc-4.2 is only provided for Tiger, as it is officially available
@@ -11,13 +9,14 @@ class TigerOnly < Requirement
 end
 
 class AppleGcc42 < Formula
+  desc 'the Apple version of the GNU Compiler Collection for OS X Tiger'
   homepage 'http://r.research.att.com/tools/'
   url 'https://ia902307.us.archive.org/31/items/tigerbrew/gcc-42-5553-darwin8-all.tar.gz'
   mirror 'http://r.research.att.com/gcc-42-5553-darwin8-all.tar.gz'
   version '4.2.1-5553'
-  sha1 '0e529a2e4723e016e3d086d6ca3215d700931503'
+  sha256 '85f4a4be48ead22b016142504f955adc2da7aa1eb1e44590263ca52f8c8a598a'
 
-  depends_on TigerOnly.new
+  depends_on TigerOnly
 
   def install
     cd 'usr' do
