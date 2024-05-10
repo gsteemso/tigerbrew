762639ee26c9aa615a641fa7df928de5dcc53f5f
Gordon Steemson <gsteemso@gmail.com>
2024-05-09 19:55:57 -0700

    brotli: went to older version that builds with our ancient CMake

--- Library/Formula/brotli.rb
+++ Library/Formula/brotli.rb
@@ -1,29 +1,33 @@
 class Brotli < Formula
   desc 'Lossless streaming compression (see RFC 7932)'
   homepage 'https://brotli.org/'
-  url 'https://github.com/google/brotli/archive/refs/tags/v1.1.0.tar.gz'
-  version '1.1.0'
-  sha256 'e720a6ca29428b803f4ad165371771f5398faba397edf6778837a18599ea13ff'
+  url 'https://github.com/google/brotli/archive/refs/tags/v1.0.9.tar.gz'
+  sha256 'f9e8d81d0405ba66d181529af42a3354f838c939095ff99930da6aa9cdf6fe46'
+
+  option :universal
 
   depends_on "cmake" => :build
 
   def install
+    ENV.universal_binary if build.universal?
     mkdir 'build-dir'
-    system 'cd', 'build-dir'
-    system 'cmake', '-DCMAKE_BUILD_TYPE=Release', "-DCMAKE_INSTALL_PREFIX=#{prefix}", '..'
+    raise
+    cd 'build-dir'
+    system 'cmake', '-Wno-dev', '-DCMAKE_BUILD_TYPE=Release', "-DCMAKE_INSTALL_PREFIX=#{prefix}", '..'
     system 'cmake', '--build', '.', '--config', 'Release', '--target', 'install'
   end
 
-  def caveats <<-_.undent
-    if Python 3 is installed, language bindings for it are available via Pip:
-      pip3 install brotli
-  _
+  def caveats
+    <<-_.undent
+      if Python 3 is installed, brotli bindings for it are available via Pip:
+        pip3 install brotli
+    _
+  end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system 'false'
+    system bin/'brotli', '-k', '-o', './brotliest.br', bin/'brotli'
+    system bin/'brotli', '-t', 'brotliest.br'
+    system bin/'brotli', '-d', 'brotliest.br'
+    system 'diff', '-s', 'brotliest', bin/'brotli'
   end
 end
