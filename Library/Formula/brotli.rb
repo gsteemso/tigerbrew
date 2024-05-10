0f0c6d8f5b78a648afe6cf845cf4efb52437eea3
Gordon Steemson <gsteemso@gmail.com>
2024-05-09 22:10:49 -0700

    brotli:  completed working formula (needs testing on Tiger)

--- Library/Formula/brotli.rb
+++ Library/Formula/brotli.rb
@@ -1,3 +1,5 @@
+# This version is the last which will build on our ancient version of CMake (all the newer CMakes
+# require C++11).
 class Brotli < Formula
   desc 'Lossless streaming compression (see RFC 7932)'
   homepage 'https://brotli.org/'
@@ -9,12 +11,24 @@ class Brotli < Formula
   depends_on "cmake" => :build
 
   def install
-    ENV.universal_binary if build.universal?
+    if build.universal?
+      ENV.permit_arch_flags
+      archs = Hardware::CPU.universal_archs
+    elsif MacOS.prefer_64_bit?
+      archs = [Hardware::CPU.arch_64_bit]
+    else
+      archs = [Hardware::CPU.arch_32_bit]
+    end
     mkdir 'build-dir'
-    raise
     cd 'build-dir'
-    system 'cmake', '-Wno-dev', '-DCMAKE_BUILD_TYPE=Release', "-DCMAKE_INSTALL_PREFIX=#{prefix}", '..'
-    system 'cmake', '--build', '.', '--config', 'Release', '--target', 'install'
+    system 'cmake', '-Wno-dev',
+                    "-DCMAKE_OSX_ARCHITECTURES='#{archs.join(';')}'",
+                    '-DCMAKE_BUILD_TYPE=Release',
+                    "-DCMAKE_INSTALL_PREFIX=#{prefix}",
+                    '..'
+    system 'cmake', '--build', '.',
+                    '--config', 'Release',
+                    '--target', 'install'
   end
 
   def caveats
