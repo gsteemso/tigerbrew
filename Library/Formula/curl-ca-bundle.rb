4b0463316abc4f0fe0897e4c1dac28d42bad6643
Gordon Steemson <gsteemso@gmail.com>
2024-05-13 21:49:04 -0700

    curl-ca-bundle: reäpplied dummy `test`, lost during rebase

--- Library/Formula/curl-ca-bundle.rb
+++ Library/Formula/curl-ca-bundle.rb
@@ -3,13 +3,8 @@ class CurlCaBundle < Formula
   homepage "http://curl.haxx.se/docs/caextract.html"
   url "https://curl.se/ca/cacert-2024-03-11.pem",
     :using => :nounzip
-<<<<<<< HEAD
-  version "2023-08-22"
-  sha256 "23c2469e2a568362a62eecf1b49ed90a15621e6fa30e29947ded3436422de9b9"
-=======
-  sha256 "1794c1d4f7055b7d02c2170337b61b48a2ef6c90d77e95444fd2596f4cac609f"
   version "2024-03-11"
->>>>>>> 97f106572f (curl-ca-bundle 2024-03-11)
+  sha256 "1794c1d4f7055b7d02c2170337b61b48a2ef6c90d77e95444fd2596f4cac609f"
 
   bottle do
     cellar :any
