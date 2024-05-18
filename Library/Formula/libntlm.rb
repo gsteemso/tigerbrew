2234d14abd2474b8be594451e455e89584d5b761
Gordon Steemson <gsteemso@gmail.com>
2024-05-18 16:06:14 -0700

    libntlm:  discovered that git is a build requirement

--- Library/Formula/libntlm.rb
+++ Library/Formula/libntlm.rb
@@ -4,10 +4,14 @@ class Libntlm < Formula
   url "https://gitlab.com/gsasl/libntlm/-/archive/v1.7/libntlm-v1.7.tar.bz2"
   sha256 "fa1c12c699f71d906b6880981cadba358e5b2e62e3d18424eaa47fa1bd9e918f"
 
+  option :universal
+
   depends_on "autoconf" => :build
+  depends_on "git" => :build
   depends_on "m4" => :build
 
   def install
+    ENV.universal_binary if build.universal?
     system "./bootstrap"
   end
 
