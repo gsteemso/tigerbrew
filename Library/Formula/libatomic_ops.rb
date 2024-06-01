b748202e563396a07ef613f9f0011b48ea898c7b
Gordon Steemson <gsteemso@gmail.com>
2024-06-01 09:48:18 -0700

    libatomic_ops:  tiny robustness edit to inline patch’s header line

--- Library/Formula/libatomic_ops.rb
+++ Library/Formula/libatomic_ops.rb
@@ -4,7 +4,7 @@ class LibatomicOps < Formula
   url "https://github.com/ivmai/libatomic_ops/releases/download/v7.8.2/libatomic_ops-7.8.2.tar.gz"
   sha256 "d305207fe207f2b3fb5cb4c019da12b44ce3fcbc593dfd5080d867b1a2419b51"
 
-  patch :p0, :DATA
+  patch :p1, :DATA
 
   option :universal
 
@@ -26,8 +26,8 @@ class LibatomicOps < Formula
 end
 
 __END__
---- tests/Makefile.in.orig     2024-03-25 11:38:47.000000000 -0700
-+++ tests/Makefile.in          2024-03-25 11:38:20.000000000 -0700
+--- old/tests/Makefile.in   2024-03-25 11:38:47.000000000 -0700
++++ new/tests/Makefile.in   2024-03-25 11:38:20.000000000 -0700
 @@ -559,7 +559,7 @@
  
  # We distribute test_atomic_include.h and list_atomic.c, since it is hard
