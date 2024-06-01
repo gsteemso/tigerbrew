45f4a90ec02e800afd766d3c7e0d8445d2cd7601
Gordon Steemson <gsteemso@gmail.com>
2024-06-01 09:54:47 -0700

    curl:  recovered edits somehow not saved earlier

--- Library/Formula/curl.rb
+++ Library/Formula/curl.rb
@@ -11,29 +11,43 @@ class Curl < Formula
 
   keg_only :provided_by_osx
 
-  option 'with-c-ares',      'Build with C-ARES asynchronous DNS support'
-  option 'with-completions', 'Build with fish and zsh command-line completions'
-  option 'with-libressl',    'Build with LibreSSL instead of OpenSSL'
-  option 'with-libssh2',     'Build with scp and sFTP support'
-
-  option 'without-gsasl',    'Build without SASL SCRAM authentication support'
+  option :universal
+  option "with-c-ares",      "Build with C-ARES asynchronous DNS support"
+  option "with-completions", "Build with fish and zsh command-line completions"
+  option 'with-gnutls',      'Build with GnuTLS instead of LibreSSL or OpenSSL'
+  option 'with-gsasl',       'Build with SASL SCRAM authentication support'
+  option "with-libressl",    "Build with LibreSSL instead of GnuTLS or OpenSSL"
+  option "with-libssh2",     "Build with scp and sFTP support"
+  option "with-upgrades",    "Build with compression, IDN, PSL, & RTMP (streaming Flash) support"
 
   deprecated_option "with-ares" => "with-c-ares"
+  deprecated_option "with-rtmp" => "with-upgrades"
+  deprecated_option "with-rtmpdump" => "with-upgrades"
   deprecated_option "with-ssh"  => "with-libssh2"
-
-  depends_on "gsasl"    => :recommended
+  deprecated_option "with-zstd" => "with-upgrades"
 
   depends_on "c-ares"   => :optional
+  depends_on 'gnutls'   => :optional
+  depends_on "gsasl"    => :optional
   depends_on "libressl" => :optional
   depends_on "libssh2"  => :optional
+  if build.with? 'upgrades'
+    depends_on 'brotli'
+    depends_on "libidn2"  # libPSL also depends on this
+    depends_on "libpsl"
+    depends_on "rtmpdump"
+    depends_on 'zstd'
+  end
 
   depends_on "libnghttp2"
-  depends_on "openssl3" if build.without? 'libressl'
+  depends_on "openssl3" if build.without? 'gnutls' and build.without? 'libressl'
   depends_on "zlib"
 
   depends_on "pkg-config" => :build
 
   def install
+    ENV.universal_binary if build.universal?
+
     # defaults we don't need to specify:
     # --disable-debug
     # --enable-optimize
@@ -42,20 +56,17 @@ class Curl < Formula
     # --enable-rtsp, --enable-proxy, --enable-dict, --enable-telnet, --enable-tftp
     # --enable-pop3, --enable-imap, --enable-smb, --enable-smtp, --enable-gopher
     # --enable-docs, --enable-manual, --enable-libcurl-option, --enable-ipv6
-    # --enable-threaded-resolver, --enable-verbose, --enable-aws, --enable-ntlm
-    # --enable-tls-srp, --enable-unix-sockets, --enable-cookies, --enable-socketpair
-    # --enable-http-auth, --enable-doh, --enable-mime, --enable-bindlocal, --enable-form-api
-    # --enable-dateparse, --enable-netrc, --enable-get-easy-options, --enable-alt-svc
-    # --enable-headers-api, --enable-hsts
-    # --with-pic, --with-libpsl, --with-libidn2, --with-libnghttp2
-    # inapplicable:
-    # --enable-ntlm-wb, --with-schannel, --with-amissl
+    # --enable-threaded-resolver, --enable-verbose, --enable-ntlm, --enable-tls-srp
+    # --enable-unix-sockets, --enable-cookies, --enable-socketpair, --enable-http-auth
+    # --enable-doh, --enable-mime, --enable-bindlocal, --enable-form-api
+    # --enable-dateparse, --enable-alt-svc, --enable-headers-api, --enable-hsts
+    # --with-libpsl, --with-libidn2, --with-libnghttp2
     # don't work:
     # --enable-symbol-hiding :  automatic, but no compiler support
     # --enable-ech :  OpenSSL build doesn’t have the API
     # --with-secure-transport :  no Tiger version & Leopard version is obsolete
     # --with-openssl-quic :  OpenSSL build doesn’t support the API (this would also provide HTTP/3)
-    # would need packages:
+    # need packages:
     # --enable-websockets
     # --with-ngtcp2 plus --with-nghttp3
     #   OR --with-quiche
@@ -64,19 +75,22 @@ class Curl < Formula
       --prefix=#{prefix}
       --disable-dependency-tracking
       --enable-mqtt
-      --enable-progress-meter
       --with-gssapi
       --with-zlib=#{Formula["zlib"].opt_prefix}
       --with-ca-fallback
     ]
-    args << (ARGV.verbose? ? '--disable-silent-rules' : '--enable-silent-rules')
+    args << (ARGV.verbose? ? "--disable-silent-rules" : "--enable-silent-rules")
 
     # cURL has a new firm desire to find ssl with PKG_CONFIG_PATH instead of using
     # "--with-ssl" any more. "when possible, set the PKG_CONFIG_PATH environment
     # variable instead of using this option". Multi-SSL choice breaks w/o using it.
-    if build.with? "libressl"
+    if build.with? 'gnutls'
+      ENV.prepend_path 'PKG_CONFIG_PATH', "#{Formula['gnutls'].opt_lib}/pkgconfig"
+      args << "--with-gnutls=#{Formula['gnutls'].opt_prefix}"
+      args << "--with-ca-bundle=#{etc}/gnutls/cert.pem"
+    elsif build.with? "libressl"
       ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["libressl"].opt_lib}/pkgconfig"
-      args << "--with-ssl=#{Formula["libressl"].opt_prefix}"
+      args << "--with-libressl=#{Formula["libressl"].opt_prefix}"
       args << "--with-ca-bundle=#{etc}/libressl/cert.pem"
     else
       ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["openssl3"].opt_lib}/pkgconfig"
@@ -86,34 +100,26 @@ class Curl < Formula
     end
 
     args << (build.with?("c-ares") ? "--enable-ares" : "--disable-ares")
-
     if build.with? 'completions'
       args << "--with-fish-functions-dir=#{fish_completion}"
       args << "--with-zsh-functions-dir=#{zsh_completion}"
     else
       args << '--without-fish-functions-dir' << '--without-zsh-functions-dir'
     end
-
     if build.with? "libssh2"
       ENV.prepend_path 'PKG_CONFIG_PATH', "#{Formula['libssh2'].opt_lib}/pkgconfig"
       args << "--with-libssh2"
     else
       args << "--without-libssh2"
     end
-
-    if Formula['brotli'].installed?
+    if build.with? "upgrades"
       ENV.prepend_path 'PKG_CONFIG_PATH', "#{Formula['brotli'].opt_lib}/pkgconfig"
-      args << '--with-brotli'
-    end
-
-    if Formula['rtmpdump'].installed?
       ENV.prepend_path 'PKG_CONFIG_PATH', "#{Formula['rtmpdump'].opt_lib}/pkgconfig"
-      args << '--with-librtmp'
-    end
-
-    if Formula['zstd'].installed?
       ENV.prepend_path 'PKG_CONFIG_PATH', "#{Formula['zstd'].opt_lib}/pkgconfig"
-      args << '--with-zstd'
+      args << '--with-brotli' << "--with-librtmp" << '--with-zstd'
+    else
+      args << '--without-brotli' << '--without-libidn2' << '--without-libpsl'
+      args << "--without-librtmp" << '--without-zstd'
     end
 
     # Tiger/Leopard ship with a horrendously outdated set of certs,
@@ -128,14 +134,6 @@ class Curl < Formula
     libexec.install "scripts/mk-ca-bundle.pl"
   end
 
-  def caveats
-    <<-_.undent
-      cURL will be built to take advantage of any or all of the following packages,
-      should they be installed:
-          brotli    libidn2    libpsl    rtmpdump    zstd
-    _
-  end
-
   test do
     # Fetch the curl tarball and see that the checksum matches.
     # This requires a network connection, but so does Homebrew in general.
@@ -145,7 +143,6 @@ class Curl < Formula
 
     # so mk-ca-bundle can find it
     ENV.prepend_path "PATH", Formula["curl"].opt_bin
-    ENV.prepend_path "PATH", Formula["perl"].opt_bin
     system libexec/"mk-ca-bundle.pl", "test.pem"
     assert File.exist?("test.pem")
     assert File.exist?("certdata.txt")
