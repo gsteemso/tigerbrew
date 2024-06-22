78e8b6542e6c2c0557e016139a5a2cb28c41fdc8
Gordon Steemson <gsteemso@gmail.com>
2024-06-22 00:00:06 -0700

    extend/leopard:  reverted mistaken consolidation of extend/module.rb

--- Library/Homebrew/extend/leopard.rb
+++ Library/Homebrew/extend/leopard.rb
@@ -70,21 +70,6 @@ class Hash
   end unless method_defined?(:first)
 end
 
-class Module
-  def attr_rw(*attrs)
-    file, line, = caller.first.split(":")
-    line = line.to_i
-
-    attrs.each do |attr|
-      module_eval <<-EOS, file, line
-        def #{attr}(val=nil)
-          val.nil? ? @#{attr} : @#{attr} = val
-        end
-      EOS
-    end
-  end unless method_defined?(:attr_rw)
-end
-
 class String
   def start_with?(*prefixes)
     prefixes.any? do |prefix|
