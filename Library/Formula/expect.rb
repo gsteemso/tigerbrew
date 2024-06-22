6bb876dafd3197a409ada2550fbd92e7f012b83a
Gordon Steemson <gsteemso@gmail.com>
2024-06-22 10:31:16 -0700

    expect:
    - added patch for rare issue encountered when reading blocking input (see
      https://debbugs.gnu.org/cgi/bugreport.cgi?bug=49078 )
    
    - tweaked URL to pass audit --strict

--- Library/Formula/expect.rb
+++ Library/Formula/expect.rb
@@ -1,7 +1,7 @@
 class Expect < Formula
   desc "Program that can automate interactive applications"
   homepage "https://core.tcl-lang.org/expect/index"
-  url "https://prdownloads.sourceforge.net/project/expect/Expect/5.45.4/expect5.45.4.tar.gz"
+  url "https://downloads.sourceforge.net/project/expect/Expect/5.45.4/expect5.45.4.tar.gz"
   sha256 "49a7da83b0bdd9f46d04a04deec19c7767bb9a323e40c4781f89caf760b92c34"
   license :public_domain
 
@@ -19,6 +19,10 @@ class Expect < Formula
     url "https://github.com/iains/darwin-expect/commit/2a98bd855e9bf2732ba6ddbd490b748d5668eeb0.patch?full_index=1"
     sha256 "deb83cfa2475b532c4e63b0d67e640a4deac473300dd986daf650eba63c4b4c0"
   end
+
+  # attempt to fix a bug encountered when blocking input is read
+  # see https://debbugs.gnu.org/cgi/bugreport.cgi?bug=49078
+  patch :DATA
 
   def install
     ENV.enable_warnings if ENV.compiler == :gcc_4_0
@@ -50,3 +54,18 @@ class Expect < Formula
     assert_match "Done", pipe_output("#{bin}/expect", "exec true; puts Done")
   end
 end
+
+__END__
+--- expect5.45.4/exp_main_sub.c	2018-02-04 10:43:58.000000000 +0000
++++ expect5.45.4/exp_main_sub.c	2021-10-23 00:39:09.375404444 +0100
+@@ -326,7 +326,9 @@
+ 
+ 	if (code != EXP_EOF) {
+ 	    inChannel = expStdinoutGet()->channel;
+-	    code = Tcl_GetsObj(inChannel, commandPtr);
++	    do {
++		code = Tcl_GetsObj(inChannel, commandPtr);
++	    } while (code < 0 && Tcl_InputBlocked(inChannel));
+ #ifdef SIMPLE_EVENT
+ 	    if (code == -1 && errno == EINTR) {
+ 		if (Tcl_AsyncReady()) {
