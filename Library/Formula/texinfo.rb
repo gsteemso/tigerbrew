0a4f9ddcdf8be69c10b47271aae93f5e9f0bb1da
Gordon Steemson <gsteemso@gmail.com>
2024-06-26 11:24:22 -0700

    texinfo:  bump version from 7.0 to 7.1

--- Library/Formula/texinfo.rb
+++ Library/Formula/texinfo.rb
@@ -1,13 +1,9 @@
 class Texinfo < Formula
   desc "Official documentation format of the GNU project"
   homepage "https://www.gnu.org/software/texinfo/"
-  url "http://ftpmirror.gnu.org/texinfo/texinfo-7.0.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/texinfo/texinfo-7.0.tar.gz"
-  sha256 "9261d4ee11cdf6b61895e213ffcd6b746a61a64fe38b9741a3aaa73125b35170"
-
-  bottle do
-    sha256 "0ed1b7cf30aa7236f562d23315f533649d15f6c01314f7a7720fe34660b7cefb" => :tiger_altivec
-  end
+  url "http://ftpmirror.gnu.org/texinfo/texinfo-7.1.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/texinfo/texinfo-7.1.tar.xz"
+  sha256 'deeec9f19f159e046fdf8ad22231981806dac332cc372f1c763504ad82b30953'
 
   keg_only :provided_by_osx, <<-EOS.undent
     Software that uses TeX, such as lilypond and octave, requires a newer version
